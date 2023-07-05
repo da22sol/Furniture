854fb0afecd129040fedfe16f06c851cd3007295
yuseon <dbtjs3@naver.com>
2023-07-06 03:31:12 +0900

    fix.전체 코드수정(임유선)

---
+++ js/main/main_side_btn.js
@@ -0,0 +1,45 @@
+/* MENU BUTTON */
+const TOMENU = document.querySelector("#btn_menu");
+const sideMenuBar = document.getElementById("side_menu_bar");
+window.addEventListener('scroll',() => {
+    if (window.scrollY > 500) {
+        gsap.to(TOMENU, 0.5, {
+            x: 0 
+        });
+    } else {
+        gsap.to(TOMENU, 0.5, {
+            x: 100 
+        });
+    }
+});
+
+TOMENU.addEventListener("click", function () {
+    sideMenuBar.style.display = "flex";
+});
+
+
+/* TOP BUTTON */
+const TOTOP = document.querySelector("#btn_top");
+window.addEventListener('scroll',() => {
+    if (window.scrollY > 500) {
+        gsap.to(TOTOP, 0.5, {
+            x: 0 
+        });
+    } else {
+        gsap.to(TOTOP, 0.5, {
+            x: 100 
+        });
+    }
+});
+
+TOTOP.addEventListener('click', function () {
+    gsap.to(window, 0.7, {
+        scrollTo: 0
+    });
+})
+
+
+export default {
+    TOMENU,
+    TOTOP,
+};
