b73b571e55e3fc259044d14c3aa70023d1fb2c5d
yuseon <dbtjs3@naver.com>
2023-07-06 14:19:16 +0900

    feat.클릭 기능 추가

--- js/category/bed.js
+++ js/category/bed.js
@@ -1,31 +1,37 @@
-let productsList = [{
-    brand: "brand 1",
-    name: "product 1",
-    price: 1000
-}, {
-    brand: "brand 2",
-    name: "product 2",
-    price: 5000
-}, {
-    brand: "brand 3",
-    name: "product 3",
-    price: 8000
-}, {
-    brand: "brand 4",
-    name: "product 4",
-    price: 20000
-}, {
-    brand: "brand 5",
-    name: "product 5",
-    price: 5000
-}, {
-    brand: "brand 6",
-    name: "product 6",
-    price: 70000
-}];
+const productsList = [
+    {
+        brand: "brand 1",
+        name: "product 1",
+        price: 1000,
+    },
+    {
+        brand: "brand 2",
+        name: "product 2",
+        price: 5000,
+    },
+    {
+        brand: "brand 3",
+        name: "product 3",
+        price: 8000,
+    },
+    {
+        brand: "brand 4",
+        name: "product 4",
+        price: 20000,
+    },
+    {
+        brand: "brand 5",
+        name: "product 5",
+        price: 5000,
+    },
+    {
+        brand: "brand 6",
+        name: "product 6",
+        price: 70000,
+    },
+];
 
-
-for ( let i = 0; i < productsList.length; i++ ) {
+for (let i = 0; i < productsList.length; i++) {
     document.getElementById("list_category_sale").innerHTML += `
     <li class="li_products">
         <a href="/">
@@ -48,17 +54,16 @@ for ( let i = 0; i < productsList.length; i++ ) {
 
 const TOTALCOUNT = document.getElementsByClassName("li_products");
 const TOTALPRODUCTS = document.querySelector(".total_products");
-let totalCount = TOTALCOUNT.length;
+const totalCount = TOTALCOUNT.length;
 
 TOTALPRODUCTS.innerText = `총 ${totalCount}개의 상품`;
 
 const FILTERSELECTED = document.querySelector(".filter_drop");
 const FILTERLIST = document.querySelector(".list_filter");
 
-FILTERSELECTED.addEventListener("click", function () {
+FILTERSELECTED.addEventListener("click", () => {
     FILTERLIST.classList.toggle("display");
 });
-
 
 export default {
     TOTALCOUNT,
@@ -67,5 +72,3 @@ export default {
     FILTERSELECTED,
     FILTERLIST,
 };
-
-
