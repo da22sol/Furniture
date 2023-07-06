a22fab3866b54201828f532c27eb5e600421cf54
yuseon <dbtjs3@naver.com>
2023-07-06 14:29:23 +0900

    chore.패키지 생성

--- js/main/main_side_btn.js
+++ js/main/main_side_btn.js
@@ -1,43 +1,41 @@
 /* MENU BUTTON */
 const TOMENU = document.querySelector("#btn_menu");
 const sideMenuBar = document.getElementById("side_menu_bar");
-window.addEventListener('scroll',() => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 500) {
         gsap.to(TOMENU, 0.5, {
-            x: 0 
+            x: 0,
         });
     } else {
         gsap.to(TOMENU, 0.5, {
-            x: 100 
+            x: 100,
         });
     }
 });
 
-TOMENU.addEventListener("click", function () {
+TOMENU.addEventListener("click", () => {
     sideMenuBar.style.display = "flex";
 });
 
-
 /* TOP BUTTON */
 const TOTOP = document.querySelector("#btn_top");
-window.addEventListener('scroll',() => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 500) {
         gsap.to(TOTOP, 0.5, {
-            x: 0 
+            x: 0,
         });
     } else {
         gsap.to(TOTOP, 0.5, {
-            x: 100 
+            x: 100,
         });
     }
 });
 
-TOTOP.addEventListener('click', function () {
+TOTOP.addEventListener("click", () => {
     gsap.to(window, 0.7, {
-        scrollTo: 0
+        scrollTo: 0,
     });
-})
-
+});
 
 export default {
     TOMENU,
