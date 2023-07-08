270a679aa9fa0ef228f6a2f5c08e0b74bfb8ce51
[SW5] 박민준 <nami47351@gmail.com>
2023-07-08 20:05:43 +0900

    fix. 변수 오타 수정

--- js/main/main_side_btn.js
+++ js/main/main_side_btn.js
@@ -1,7 +1,7 @@
 /* MENU BUTTON */
-const toMenu = document.querySelector("#btn_menu");
-const sideMenuBar = document.getElementById("side_menu_bar");
-window.addEventListener("scroll", () => {
+const toMenu = document.querySelector('#btn_menu');
+const sideMenuBar = document.getElementById('side_menu_bar');
+window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
         gsap.to(toMenu, 0.5, {
             x: 0,
@@ -13,13 +13,13 @@ window.addEventListener("scroll", () => {
     }
 });
 
-toMenu.addEventListener("click", () => {
-    sideMenuBar.style.display = "flex";
+toMenu.addEventListener('click', () => {
+    sideMenuBar.style.display = 'flex';
 });
 
 /* TOP BUTTON */
-const toTop = document.querySelector("#btn_top");
-window.addEventListener("scroll", () => {
+const toTop = document.querySelector('#btn_top');
+window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
         gsap.to(toTop, 0.5, {
             x: 0,
@@ -31,7 +31,7 @@ window.addEventListener("scroll", () => {
     }
 });
 
-TOTOP.addEventListener("click", () => {
+toTop.addEventListener('click', () => {
     gsap.to(window, 0.7, {
         scrollTo: 0,
     });
