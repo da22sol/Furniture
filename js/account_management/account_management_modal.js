b1a984c84bc79ae773858f0c960de53ff4e740ea
[SW5] 박민준 <nami47351@gmail.com>
2023-07-11 19:43:31 +0900

    feat. 카테고리, 상품목록 기능 추가

---
+++ js/account_management/account_management_modal.js
@@ -0,0 +1,6 @@
+// const modalUserCheck = document.querySelector('#modal_usercheck');
+// const modalOutBtn = document.querySelector('.btn_close_modal');
+
+// modalOutBtn.addEventListener('click', () => {
+//     modalUserCheck.style.display = 'none';
+// });
