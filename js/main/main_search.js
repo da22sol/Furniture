41649ec731c71b6c874d4edc60361ed123a0ce90
yuseon <dbtjs3@naver.com>
2023-07-14 02:21:11 +0900

    remove.main_search.js 삭제

--- js/main/main_search.js
+++
@@ -1,6 +0,0 @@
-const searchEl = document.querySelector('.icon_search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-});
