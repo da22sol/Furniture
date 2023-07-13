f39f011e2cf19d68aa62dac8c094c0b84e5312bf
[SW5] 박민준 <nami47351@gmail.com>
2023-07-13 18:50:03 +0900

    remove. 불필요한 파일 삭제

--- js/account_management/account_management_modal.js
+++
@@ -1,6 +0,0 @@
-// const modalUserCheck = document.querySelector('#modal_usercheck');
-// const modalOutBtn = document.querySelector('.btn_close_modal');
-
-// modalOutBtn.addEventListener('click', () => {
-//     modalUserCheck.style.display = 'none';
-// });
