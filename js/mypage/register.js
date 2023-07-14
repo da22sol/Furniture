2dac0fc5d34a247e76b87ba7fb0b288c72559525
yuseon <dbtjs3@naver.com>
2023-07-15 08:33:04 +0900

    fix.유효성 검사 수정

--- js/mypage/register.js
+++ js/mypage/register.js
@@ -14,19 +14,17 @@ const joinZipcodeInput = document.getElementById("zipcode");
 const joinAddrInput1 = document.getElementById("addr1");
 const joinAddrInput2 = document.getElementById("addr2");
 
-
 // 휴대전화 input 자동 focus
-joinPhoneInput1.addEventListener("input", function() {
+joinPhoneInput1.addEventListener("input", function () {
     if (joinPhoneInput1.value.length >= joinPhoneInput1.maxLength) {
         joinPhoneInput2.focus();
     }
 });
-joinPhoneInput2.addEventListener("input", function() {
+joinPhoneInput2.addEventListener("input", function () {
     if (joinPhoneInput2.value.length >= joinPhoneInput2.maxLength) {
         joinPhoneInput3.focus();
     }
 });
-
 
 // 주소 검색 api
 joinBtn.addEventListener("click", () => {
@@ -85,6 +83,12 @@ function handleSubmit(e) {
     const address1 = joinAddrInput1.value;
     const address2 = joinAddrInput2.value;
 
+    // 비밀번호 비교
+    if (password !== joinUserPwConfirm) {
+        alert("❗️비밀번호가 일치하지 않습니다.");
+        joinPasswordConfirmInput.focus();
+    }
+
     // 유효성 검사
     if (fullName == "") {
         alert("❗️이름을 입력해 주세요.");
@@ -109,45 +113,40 @@ function handleSubmit(e) {
         joinPhoneInput3.focus();
     } else if (address1 == "" || address2 == "") {
         alert("❗️주소를 입력해주세요.");
+    } else {
+        // 객체
+        const userInfoData = {
+            email: email,
+            password: password,
+            fullName: fullName,
+            phoneNumber: phoneNumber,
+            address: {
+                postalCode: postalCode,
+                address1: address1,
+                address2: address2,
+            },
+        };
+
+        const dataJson = JSON.stringify(userInfoData);
+
+        fetch("http://kdt-sw-5-team01.elicecoding.com/api/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: dataJson,
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.result == "success-register") {
+                    // 회원가입 완료 모달창 띄우기
+                    joinModal.style.display = "flex";
+                    document.getElementsByClassName(
+                        "welcome_user_name",
+                    )[0].innerText += `${fullName}님 `;
+                } else {
+                    alert("❗️회원가입 실패");
+                }
+            });
     }
-
-    // 비밀번호 비교
-    if (password !== joinUserPwConfirm) {
-        alert("❗️비밀번호가 일치하지 않습니다.");
-        joinPasswordConfirmInput.focus();
-    }
-
-    // 객체
-    const userInfoData = {
-        email: email,
-        password: password,
-        fullName: fullName,
-        phoneNumber: phoneNumber,
-        address: {
-            postalCode: postalCode,
-            address1: address1,
-            address2: address2,
-        },
-    };
-
-    const dataJson = JSON.stringify(userInfoData);
-
-    fetch("http://kdt-sw-5-team01.elicecoding.com/api/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: dataJson,
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        if(data.result == "success-register") {
-            // 회원가입 완료 모달창 띄우기
-            joinModal.style.display = "flex";
-            document.getElementsByClassName("welcome_user_name")[0].innerText += `${fullName}님 `;
-        } else {
-            alert("❗️회원가입 실패");
-        }
-    });
 }
