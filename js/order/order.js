d2c6ca51904881d6e121e2d3c77051a3b7f845ce
[SW5] 박민준 <nami47351@gmail.com>
2023-07-28 23:19:21 +0900

    "add. 네이버페이 주문하기 추가"

--- js/order/order.js
+++ js/order/order.js
@@ -1,25 +1,25 @@
 // 배송지
-const orderAddr = document.getElementsByClassName("addr_search_zipcode")[0];
-const receiverNameInput = document.getElementsByClassName("addr_info_name")[0];
-const receiverPhoneInput1 = document.getElementsByClassName("addr_phone1")[0];
-const receiverPhoneInput2 = document.getElementsByClassName("addr_phone2")[0];
-const receiverPhoneInput3 = document.getElementsByClassName("addr_phone3")[0];
-const zipcodeInput = document.getElementsByClassName("addr_zipcode")[0];
-const addrInput1 = document.getElementsByClassName("addr_addr1")[0];
-const addrInput2 = document.getElementsByClassName("addr_addr2")[0];
+const orderAddr = document.getElementsByClassName('addr_search_zipcode')[0];
+const receiverNameInput = document.getElementsByClassName('addr_info_name')[0];
+const receiverPhoneInput1 = document.getElementsByClassName('addr_phone1')[0];
+const receiverPhoneInput2 = document.getElementsByClassName('addr_phone2')[0];
+const receiverPhoneInput3 = document.getElementsByClassName('addr_phone3')[0];
+const zipcodeInput = document.getElementsByClassName('addr_zipcode')[0];
+const addrInput1 = document.getElementsByClassName('addr_addr1')[0];
+const addrInput2 = document.getElementsByClassName('addr_addr2')[0];
 
 // 주문
-const orderGoodsList = document.getElementsByClassName("product_pay")[0];
-const orderTotalPrice = document.getElementsByClassName("total_price")[0];
-const userOrderInfo = document.getElementsByClassName("user_order_info")[0];
+const orderGoodsList = document.getElementsByClassName('product_pay')[0];
+const orderTotalPrice = document.getElementsByClassName('total_price')[0];
+const userOrderInfo = document.getElementsByClassName('user_order_info')[0];
 
 // 현재 로그인 되어 있는 계정 토큰 불러오기
-const USERTOKEN = localStorage.getItem("userToken");
+const USERTOKEN = localStorage.getItem('userToken');
 
-fetch("http://kdt-sw-5-team01.elicecoding.com/api/account", {
-    method: "GET",
+fetch('http://kdt-sw-5-team01.elicecoding.com/api/account', {
+    method: 'GET',
     headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${USERTOKEN}`,
     },
 })
@@ -44,9 +44,8 @@ fetch("http://kdt-sw-5-team01.elicecoding.com/api/account", {
         addrInput2.value += `${loginUserdata.address.address2}`;
 
         // 장바구니 물건 가져오기
-        const userOrderFinList = JSON.parse(localStorage.getItem("cartItems"));
-        console.log(userOrderFinList)
-    
+        const userOrderFinList = JSON.parse(localStorage.getItem('cartItems'));
+
         // 상품 주문하기
         let totalQuan = 0;
         let totalSum = 0;
@@ -71,12 +70,12 @@ fetch("http://kdt-sw-5-team01.elicecoding.com/api/account", {
                     <div class="total_box">
                         <p class="price_total">${userOrderFinList[
                             i
-                        ].price.toLocaleString("ko-KR")}원</p>
+                        ].price.toLocaleString('ko-KR')}원</p>
                         <span class="count_total">${userOrderFinList[
                             i
-                        ].quantity.toLocaleString("ko-KR")}개</span>
+                        ].quantity.toLocaleString('ko-KR')}개</span>
                         <p class="all_total">${sum.toLocaleString(
-                            "ko-KR",
+                            'ko-KR',
                         )}원</p>
                     </div>
                 </li>
@@ -87,19 +86,19 @@ fetch("http://kdt-sw-5-team01.elicecoding.com/api/account", {
 
         // 총 결제 금액
         orderTotalPrice.innerHTML += `
-    <span>${totalSum.toLocaleString("ko-KR")}원</span>
+    <span>${totalSum.toLocaleString('ko-KR')}원</span>
 `;
 
         // 주문하기 클릭
 
-        const purchaseBtn = document.getElementsByClassName("purchase")[0];
+        const purchaseBtn = document.getElementsByClassName('purchase')[0];
 
-        purchaseBtn.addEventListener("click", () => {
+        purchaseBtn.addEventListener('click', () => {
             // 객체
             const userOrderDetail = {
                 user: loginUserdata._id,
                 totalPrice: totalSum,
-                status: "주문완료",
+                status: '주문완료',
             };
 
             const orderDataJson = JSON.stringify(userOrderDetail);
@@ -111,18 +110,17 @@ fetch("http://kdt-sw-5-team01.elicecoding.com/api/account", {
             // console.log(productNameList)
 
             // 주문 정보 보내기
-            fetch("http://kdt-sw-5-team01.elicecoding.com/api/orderslist", {
-                method: "POST",
+            fetch('http://kdt-sw-5-team01.elicecoding.com/api/orderslist', {
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",
+                    'Content-Type': 'application/json',
                     Authorization: `Bearer ${USERTOKEN}`,
                 },
                 body: orderDataJson,
             })
                 .then((res) => res.json())
                 .then((orderData) => {
-                    
-                    if (orderData.status == "주문완료") {
+                    if (orderData.status == '주문완료') {
                         // 주문 상품 리스트 가져오기
 
                         // 주문 상세 정보 보내기
@@ -138,45 +136,51 @@ fetch("http://kdt-sw-5-team01.elicecoding.com/api/account", {
 
                         const orderDetailDataJson =
                             JSON.stringify(userOrderDetailInfo);
-                        
 
                         fetch(
-                            "http://kdt-sw-5-team01.elicecoding.com/api/ordersitem",
+                            'http://kdt-sw-5-team01.elicecoding.com/api/ordersitem',
                             {
-                                method: "POST",
+                                method: 'POST',
                                 headers: {
-                                    "Content-Type": "application/json",
+                                    'Content-Type': 'application/json',
                                     Authorization: `Bearer ${USERTOKEN}`,
                                 },
                                 body: orderDetailDataJson,
                             },
                         )
                             .then((res) => res.json())
-                            .then((orderDetailData) => {
-                            });
+                            .then((orderDetailData) => {});
 
                         // 주문완료 모달창 띄우기
-                        orderFinModal.style.display = "block";
-                        localStorage.removeItem("cartItems");
-                        
+                        orderFinModal.style.display = 'block';
+                        localStorage.removeItem('cartItems');
                     } else {
-                        alert("주문 실패");
+                        alert('주문 실패');
                     }
                 });
+        });
+
+        // 네이버페이로 주문하기 클릭
+
+        const purchaseNaverBtn =
+            document.getElementsByClassName('purchase_naver')[0];
+
+        purchaseNaverBtn.addEventListener('click', () => {
+            location.href = '../../html/paymentNaver2.html';
         });
     });
 
 // 주문완료 모달창 (diplay = "none")
-const orderFinModal = document.getElementById("order_success");
+const orderFinModal = document.getElementById('order_success');
 
 // 장바구니 페이지로 이동
-const moveCartBtn = document.getElementsByClassName("movecart")[0];
-moveCartBtn.addEventListener("click", () => {
-    location.href = "/html/cart.html";
+const moveCartBtn = document.getElementsByClassName('movecart')[0];
+moveCartBtn.addEventListener('click', () => {
+    location.href = '/html/cart.html';
 });
 
 // 주문 완료 후 주문 조회 페이지로 이동
-const orderModalBtn = document.getElementsByClassName("btn_order_success")[0];
-orderModalBtn.addEventListener("click", () => {
-    location.href = "/html/order_tracking.html";
+const orderModalBtn = document.getElementsByClassName('btn_order_success')[0];
+orderModalBtn.addEventListener('click', () => {
+    location.href = '/html/order_tracking.html';
 });
