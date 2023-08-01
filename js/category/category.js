cc480880f9138656075f694e5b5d056e97cc8920
[SW5] 박민준 <nami47351@gmail.com>
2023-08-01 16:38:56 +0900

    fix. 카테고리별 필터 기능 수정

--- js/category/category.js
+++ js/category/category.js
@@ -6,17 +6,19 @@ fetch('http://kdt-sw-5-team01.elicecoding.com/api/categories')
         categoryMade();
     });
 
+// 카테고리 목록 배열
 let categorysListArr = [];
 
-const categoryList = document.querySelector('.category_list');
+// 카테고리 담을 리스트
+const categoryList = document.querySelector('#category_list');
 
-// 카테고리 동적 추가 함수
+// 카테고리 동적 추가
 function categoryMade() {
     for (let i = 0; i < categorysListArr.length; i++) {
         categorysListArr[
             i
         ].imgUrl = `../assets/img/${categorysListArr[i].title}.png`;
-        document.getElementById('category_list').innerHTML += `
+        categoryList.innerHTML += `
         <li class="category_list" id="${categorysListArr[i].title}">
             <a href="./category.html" class="list_category">
                 <span class="img_category">
@@ -28,103 +30,38 @@ function categoryMade() {
         `;
     }
 
-    // 제품의 총 개수 구하기
-    const totalProducts = document.querySelector('.total_products');
+    // 분류된 카테고리 상품 구분
+    const categoryListItems = document.querySelectorAll('.category_list');
 
-    function totalProduts() {
-        return (totalProducts.innerText = `총 ${
-            document.getElementsByClassName('li_products').length
-        }개의 상품`);
-    }
-    totalProduts();
+    categoryListItems.forEach((item) =>
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
 
-    // 카테고리별 제품의 총 개수 구하기
-    function totalCategorys(len) {
-        return (totalProducts.innerText = `총 ${len}개의 상품`);
-    }
+            const goodsListArr = productsListArr.filter(
+                (data) => data.categoryId.title === event.currentTarget.id,
+            );
 
-    // 동적으로 생성한 카테고리 버튼
-    const bedBtn = document.getElementById('bed');
-    const sofaBtn = document.getElementById('sofa');
-    const tableBtn = document.getElementById('table');
-    const closetBtn = document.getElementById('closet');
-    const drawerBtn = document.getElementById('drawer');
-    const deskBtn = document.getElementById('desk');
+            productReset();
+            productMade(goodsListArr);
 
-    // 침대 카테고리 상품 분류
-    bedBtn.addEventListener('click', (e) => {
-        const bedListArr = productsListArr.filter(
-            (data) => data.categoryId.title === 'bed',
-        );
-        const bedCountLen = bedListArr.length;
+            // 분류된 카테고리 상품 중 필터링 기능
+            listFilterNew.addEventListener('click', () => {
+                filterSelectesd.innerHTML = '신상품순';
+                listFilterNewDateSort(goodsListArr);
+            });
 
-        e.preventDefault();
-        productReset();
-        productMade(bedListArr);
-        totalCategorys(bedCountLen);
-    });
+            listFilterHighPrice.addEventListener('click', () => {
+                filterSelectesd.innerHTML = '높은가격순';
+                listFilterHighPriceSort(goodsListArr);
+            });
 
-    // 소파 카테고리 상품 분류
-    sofaBtn.addEventListener('click', (e) => {
-        const sofaListArr = productsListArr.filter(
-            (data) => data.categoryId.title === 'sofa',
-        );
-        const sofaCountLen = sofaListArr.length;
+            listFilterLowPrice.addEventListener('click', () => {
+                filterSelectesd.innerHTML = '낮은가격순';
+                listFilterLowPriceSort(goodsListArr);
+            });
 
-        e.preventDefault();
-        productReset();
-        productMade(sofaListArr);
-        totalCategorys(sofaCountLen);
-    });
-
-    // 식탁 카테고리 상품 분류
-    tableBtn.addEventListener('click', (e) => {
-        const tableListArr = productsListArr.filter(
-            (data) => data.categoryId.title === 'table',
-        );
-        const tableCountLen = tableListArr.length;
-
-        e.preventDefault();
-        productReset();
-        productMade(tableListArr);
-        totalCategorys(tableCountLen);
-    });
-
-    // 옷장 카테고리 상품 분류
-    closetBtn.addEventListener('click', (e) => {
-        const closetListArr = productsListArr.filter(
-            (data) => data.categoryId.title === 'closet',
-        );
-        const closetCountLen = closetListArr.length;
-
-        e.preventDefault();
-        productReset();
-        productMade(closetListArr);
-        totalCategorys(closetCountLen);
-    });
-
-    // 협탁 카테고리 상품 분류
-    drawerBtn.addEventListener('click', (e) => {
-        const drawerListArr = productsListArr.filter(
-            (data) => data.categoryId.title === 'drawer',
-        );
-        const drawerCountLen = drawerListArr.length;
-
-        e.preventDefault();
-        productReset();
-        productMade(drawerListArr);
-        totalCategorys(drawerCountLen);
-    });
-
-    // 책상 카테고리 상품 분류
-    deskBtn.addEventListener('click', (e) => {
-        const deskListArr = productsListArr.filter(
-            (data) => data.categoryId.title === 'desk',
-        );
-        const deskCountLen = deskListArr.length;
-        e.preventDefault();
-        productReset();
-        productMade(deskListArr);
-        totalCategorys(deskCountLen);
-    });
+            // 필터링 default 값
+            filterSelectesd.innerHTML = '신상품순';
+        }),
+    );
 }
--- js/category/goods.js
+++ js/category/goods.js
@@ -1,3 +1,4 @@
+// 상품 정보 불러오기
 fetch('http://kdt-sw-5-team01.elicecoding.com/api/products')
     .then((response) => response.json())
     .then((data) => {
@@ -5,20 +6,19 @@ fetch('http://kdt-sw-5-team01.elicecoding.com/api/products')
         productMade(productsListArr);
     });
 
-// 상품 목록 객체 배열
+// 상품 목록 배열
 let productsListArr = [];
-const listFilterNew = document.getElementById('list_filter_new');
-const listFilterHighPrice = document.getElementById('list_filter_high');
-const listFilterLowPrice = document.getElementById('list_filter_low');
 
-// 상품 동적 추가 함수
+// 상품 동적 추가
 function productMade(data) {
     for (let i = 0; i < data.length; i++) {
         document.getElementById('list_category_sale').innerHTML += `
         <li class="li_products">
             <a id="${data[i]._id}" href="/html/detail.html?${data[i]._id}">
                 <span class="img_sale_product">
-                    <img src="${data[i].productImageKey}" class="img_sale_product"></img>
+                    <img src="${
+                        data[i].productImageKey
+                    }" class="img_sale_product"></img>
                 </span>
                 <strong>
                     ${data[i].categoryId.title}
@@ -27,30 +27,80 @@ function productMade(data) {
                     ${data[i].productName}
                 </p>
                 <p>
-                    ${data[i].price.toLocaleString("ko-KR")}원
+                    ${data[i].price.toLocaleString('ko-KR')}원
                 </p>
             </a>
         </li>
         `;
     }
 
+    // 상품 개수 세기
     const totalCount = document.getElementsByClassName('li_products');
     const totalProducts = document.querySelector('.total_products');
     const totalCountLen = totalCount.length;
 
-    // 제품의 총 개수 구하기
+    // 제품의 총개수 구하기
     function totalProduts() {
         return (totalProducts.innerText = `총 ${totalCountLen}개의 상품`);
     }
     totalProduts();
-
-    // 카테고리별 제품의 총 개수 구하기
-    function totalCategorys(len) {
-        return (totalProducts.innerText = `총 ${len}개의 상품`);
-    }
 }
 
-// 상품 초기화 함수
+// 상품 초기화 기능
 function productReset() {
     document.getElementById('list_category_sale').innerHTML = '';
 }
+
+// 필터 목록
+const filterDrop = document.querySelector('.filter_drop');
+const filterSelectesd = document.querySelector('.filter_selected');
+const filterList = document.querySelector('.list_filter');
+
+// 필터 버튼
+const listFilterNew = document.getElementById('list_filter_new');
+const listFilterHighPrice = document.getElementById('list_filter_high');
+const listFilterLowPrice = document.getElementById('list_filter_low');
+
+// 필터 보이기 기능
+filterDrop.addEventListener('click', () => {
+    filterList.classList.toggle('display');
+});
+
+// 신상품순 동적 변경
+function listFilterNewDateSort(data) {
+    const newProductsListArr = [...data];
+    newProductsListArr.sort((a, b) => a.createdAt - b.createdAt);
+    productReset();
+    productMade(newProductsListArr);
+}
+
+// 높은 가격순 동적 변경
+function listFilterHighPriceSort(data) {
+    const newProductsListArr = [...data];
+    newProductsListArr.sort((a, b) => b.price - a.price);
+    productReset();
+    productMade(newProductsListArr);
+}
+
+// 낮은 가격순 동적 변경
+function listFilterLowPriceSort(data) {
+    const newProductsListArr = [...data];
+    newProductsListArr.sort((a, b) => a.price - b.price);
+    productReset();
+    productMade(newProductsListArr);
+}
+
+listFilterNew.addEventListener('click', () => {
+    filterSelectesd.innerHTML = '신상품순';
+    listFilterNewDateSort(productsListArr);
+});
+
+listFilterHighPrice.addEventListener('click', () => {
+    filterSelectesd.innerHTML = '높은가격순';
+    listFilterHighPriceSort(productsListArr);
+});
+
+listFilterLowPrice.addEventListener('click', () => {
+    filterSelectesd.innerHTML = '낮은가격순';
+    listFilterLowPriceSort(productsListArr);
+});
